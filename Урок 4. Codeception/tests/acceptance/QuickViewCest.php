3d4be83166eed82fa18280ff351b420befc6147f
dea-a <darya.a.lapteva@gmail.com>
2021-05-27 10:18:56 +0600

    Modified acceptance the test. Added locators for acceptance and functional tests.

--- Урок 4. Codeception/tests/acceptance/QuickViewCest.php
+++ Урок 4. Codeception/tests/acceptance/QuickViewCest.php
@@ -6,6 +6,28 @@ class QuickViewCest
     // Открытие страницы и быстрый просмотр товара с момощью кнопки "quick view"
     public function ProductQuickView(AcceptanceTester $I)
     {
+
+        //Переменные для локаторов CSS и XPath
+
+        $tabContentCss = '.tab-content';
+        $tabContentXPath = '//div[@class="tab-content"]';
+        $homefeaturedCss = '#homefeatured.product_list ';
+        $homefeaturedXPath = '//ul[@id ="homefeatured"]';
+        $productNameCss = '#homefeatured > li:nth-child(2) h5 > a';
+        $productNameXPath = '//ul[@id="homefeatured"]/li[2]//h5/a';
+        $quickViewButtonCss = '#homefeatured > li:nth-child(2) a.quick-view';
+        $quickViewButtonXPath = '//ul[@id="homefeatured"]/li[2]//div[1]/div/a[2]';
+        $iframeNameCss = '.fancybox-iframe';
+        $iframeNameXPath = '//iframe[@class="fancybox-iframe"]';
+        $productCss = '#product';
+        $productXPath = '//*[@id="product"]';
+        $productCenterColumCss = '#product div.pb-center-column.col-xs-12.col-sm-4';
+        $productCenterColumXPath = '//*[@id="product"]//div/div[2]';
+        $productNameForQuickViewCss = '#product div.pb-center-column.col-xs-12.col-sm-4 > h1';
+        $productNameForQuickViewXPath = '//*[@id="product"]//div/div[2]/h1';
+
+        //шаги для выполнения
+
         $I->amOnPage('');
         $I->seeElement('#center_column > div.tab-content');
         $I->seeElement('#homefeatured');
@@ -13,9 +35,10 @@ class QuickViewCest
         $I->moveMouseOver('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.product_img_link');
         $I->seeElement('//*[@id="homefeatured"]/li[2]/div/div[1]/div/a[2]');
         $I->click('//*[@id="homefeatured"]/li[2]/div/div[1]/div/a[2]');
-        $I->waitForElement('.primary_block row');
-        $I->seeElement('.primary_block row');
-    
+        $I->switchToIFrame('.fancybox-iframe');
+        $I->seeElement('#product');
+        $I->seeElement('#product > div > div > div.pb-center-column.col-xs-12.col-sm-4');
+
         codecept_debug($I->grabTextFrom('#product > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1'));
 
         $I->see('Blouse', '#product > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1');
--- Урок 4. Codeception/tests/functional/SearchCest.php
+++ Урок 4. Codeception/tests/functional/SearchCest.php
@@ -5,6 +5,23 @@ class SearchCest
     // Поиск товара с помощью поисковой строки и подсчет количества найденных товаров
     public function SearchAndCountTheProducts(FunctionalTester $I)
     {
+        //Переменные для локаторов CSS и XPath
+
+        $searchBlockTopCss = '.col-sm-4#search_block_top';
+        $searchBlockTopXPath = '//div[@id="search_block_top"]';
+        $searchQueryTopCss = '.search_query';
+        $searchQueryTopXPath = '//input[@id="search_query_top"]';
+        $searchButtonCss = '#searchbox > .button-search';
+        $searchButtonXPath = '//button[contains (@class, "button-search")]'; // '//form[@id="searchbox"]/button'
+        $columnsWithProductCss = '.columns-container';
+        $columnsWithProductXPath = '//div[@id="columns"]';
+        $productListCss = '.product_list';
+        $productListXPath = '//div[@id="center_column"]/ul';
+        $productContainerCss = '.product-container';
+        $productContainerXPath = '//div[@class="product-container"]';
+
+        //шаги для выполнения
+
         $I->amOnPage('');
         $I->seeElement('#search_block_top');
         $I->seeElement('#search_query_top');
